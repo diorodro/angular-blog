7f4297de3ca02d90efcfdd596d1563daf5c4ae9c
Pichau <rodrigosantiagodionizio@hotmail.com>
2024-03-02 16:35:22 -0300

    first commit

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,34 @@
+export const data = [
+  {
+    id: '1',
+    photo: 'https://m.media-amazon.com/images/I/51pPv5GHOCL._AC_.jpg',
+    title: 'Título da Notícia',
+    description: 'Descrição da notícia',
+  },
+  {
+    id: '2',
+    photo: 'https://m.media-amazon.com/images/I/81qWTOQj0fL.jpg',
+    title: 'Título da Notícia',
+    description: 'Descrição da notícia',
+  },
+  {
+    id: '3',
+    photo:
+      'https://m.media-amazon.com/images/I/71HEcHGQ3+L._AC_UF894,1000_QL80_.jpg',
+    title: 'Título da Notícia',
+    description: 'Descrição da notícia',
+  },
+  {
+    id: '4',
+    photo: 'https://m.media-amazon.com/images/I/315MDaLENGL._AC_.jpg',
+    title: 'Título da Notícia',
+    description: 'Descrição da notícia',
+  },
+  {
+    id: '5',
+    photo:
+      'https://www.manhattantoy.com/cdn/shop/products/Collection-Lankies_Group_51dbd5a1-338e-4faf-ac62-56ee3c8c9da4.jpg?v=1673025541&width=256',
+    title: 'Título da Notícia',
+    description: 'Descrição da notícia',
+  },
+];
